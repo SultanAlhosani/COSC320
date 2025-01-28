6b97fc46e55cdca4598a852d9571a60dff0f6512
SultanAlhosani <smashyblaze@gmail.com>
2025-01-28 14:21:33 +0400

    Add files via upload

---
+++ move_semantics/move_semantics1.rs
@@ -0,0 +1,27 @@
+// TODO: Fix the compiler error in this function.
+fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec;
+
+    vec.push(88);
+
+    vec
+}
+
+fn main() {
+    let vec0 = vec![22, 44, 66]; // Initial vector
+    let vec1 = fill_vec(vec0);   // Call `fill_vec` and store the result
+
+    println!("Updated vector: {:?}", vec1); // Output the modified vector
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn move_semantics1() {
+        let vec0 = vec![22, 44, 66];
+        let vec1 = fill_vec(vec0);
+        assert_eq!(vec1, vec![22, 44, 66, 88]);
+    }
+}
